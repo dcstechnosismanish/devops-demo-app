014465d5dac78ff4aeaee8842ad4b7132b36e16d
dcstechnosismanish <dcstechnosismanish@gmail.com>
2026-07-16 08:48:18 +0000

    Setup Node.js in GitHub Actions

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log("Welcome to DevOps Demo App");
+console.log("🚀 Welcome to DevOps Demo App");
+console.log("Running inside GitHub Actions");
+console.log("Node Version:", process.version);
